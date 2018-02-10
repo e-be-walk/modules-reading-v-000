1f09f1b94c9a8816e50e3cdd3c6f3868533a5bf3
Erin Walker <erinb0317@hotmail.com>
2018-02-10 23:44:29 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance 
+  include Dance
+
+  def initialize(name)
+    @name = name
+  end
 end
