12915a1bce7216b59dfddb14a46a00ab4046dc9b
Erin Walker <erinb0317@hotmail.com>
2018-02-11 00:06:50 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,7 @@
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend MetaDancing::ClassMethods
+  extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
   attr_accessor :name
